bc64a72f31e9d0ca0de3b35d389d208a89574155
chrome mono <chromemono@wirelessprv-10-193-98-209.near.illinois.edu>
2023-10-20 13:44:22 -0500

    update on if

--- mp4/hdl/if_stage/if.sv
+++ mp4/hdl/if_stage/if.sv
@@ -20,12 +20,12 @@ pc PC (
     .rst(rst), //may need flsuh
     .load(load_pc), //may use for stall
     .in(pcmux_out),
-    .out(pc_out)
+    .out(if_output.pc_out)
 );
 
 always_comb begin : PC_MUX
     unique case (pcmux_sel)
-        pcmux::pc_plus4: pcmux_out = pc_out + 4;
+        pcmux::pc_plus4: pcmux_out = if_output.pc_out + 4;
         pcmux::alu_out: pcmux_out = alu_out;
         pcmux::alu_mod2: pcmux_out = {alu_out[31:1], 1'b0};
     endcase
