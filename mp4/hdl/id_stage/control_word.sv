9ac2e8c5b1c26447bb0897d7a6b4d8658610ffc6
MeanPaper <ldm1833991255@gmail.com>
2023-10-20 00:45:22 -0500

    tried to avoid using functions

--- mp4/hdl/id_stage/control_word.sv
+++ mp4/hdl/id_stage/control_word.sv
@@ -35,22 +35,18 @@ assign store_funct3 = `store_funct3_t(funct3);
 assign load_funct3 = `load_funct3_t(funct3);
 assign arith_funct3 = `arith_funct3_t(funct3);
 
-
-
-function void setALU(alumux::alumux1_sel_t sel1, alumux::alumux2_sel_t sel2, alu_ops op); 
-    ex_ctrls.alumux1_sel = sel1;
-    ex_ctrls.alumux2_sel = sel2;
-    ex_ctrls.aluop = op;
-endfunction
-
-function void setCMP(cmpmux::cmpmux_sel_t sel, branch_funct3_t op);
-    ex_ctrls.cmpmux_sel = sel;
-    ex_ctrls.cmpop = op;
-endfunction
-
-function void setRegfileMux(regfilemux::regfilemux_sel_t sel);
-    wb_ctrls.regfilemux_sel = sel;
-endfunction
+// function void setALU(alumux::alumux1_sel_t sel1, alumux::alumux2_sel_t sel2, alu_ops op); 
+//     ex_ctrls.alumux1_sel = sel1;
+//     ex_ctrls.alumux2_sel = sel2;
+//     ex_ctrls.aluop = op;
+// endfunction
+// function void setCMP(cmpmux::cmpmux_sel_t sel, branch_funct3_t op);
+//     ex_ctrls.cmpmux_sel = sel;
+//     ex_ctrls.cmpop = op;
+// endfunction
+// function void setRegfileMux(regfilemux::regfilemux_sel_t sel);
+//     wb_ctrls.regfilemux_sel = sel;
+// endfunction
 
 
 // i_type instruction, or op_imm will write to register
@@ -59,11 +55,15 @@ function set_op_imm_ctrl();
     wb_ctrls.load_regfile = 1'b1;
     unique case(arith_funct3) // arithmetic operation are encoded in funct3
         slt: begin
-            setCMP(cmpmux::i_imm, blt);
+            // setCMP(cmpmux::i_imm, blt);
+            ex_ctrls.cmpmux_sel = cmpmux::i_imm;
+            ex_ctrls.cmpop = blt;
             wb_ctrls.regfilemux_sel = regfilemux::br_en;
         end
         sltu: begin
-            setCMP(cmpmux::i_imm, bltu);
+            // setCMP(cmpmux::i_imm, bltu);
+            ex_ctrls.cmpmux_sel = cmpmux::i_imm;
+            ex_ctrls.cmpop = bltu;
             wb_ctrls.regfilemux_sel = regfilemux::br_en; 
         end
         sr: begin
@@ -73,11 +73,16 @@ function set_op_imm_ctrl();
             if(funct7[5] == 1'b1) begin             // funct7[5] == 1
                 ex_ctrls.aluop = alu_sra;
             end
-            setRegfileMux(regfilemux::alu_out);
+            wb_ctrls.regfilemux_sel = regfilemux::alu_out;
+            // setRegfileMux(regfilemux::alu_out);
         end
         default: begin  // add, and, or, xor, sll
-            setALU(alumux::rs1_out, alumux::i_imm, `alu_ops(arith_funct3)); // EX
-            setRegfileMux(regfilemux::alu_out); // WB 
+                ex_ctrls.alumux1_sel = alumux::rs1_out;
+                ex_ctrls.alumux2_sel = alumux::i_imm;
+                ex_ctrls.aluop = `alu_ops(arith_funct3);
+                wb_ctrls.regfilemux_sel = regfilemux::alu_out;
+            // setALU(alumux::rs1_out, alumux::i_imm, `alu_ops(arith_funct3)); // EX
+            // setRegfileMux(regfilemux::alu_out); // WB 
         end
     endcase
     
