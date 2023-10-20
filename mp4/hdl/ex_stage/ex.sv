bd18e4a7544cbbe9d5c7a755a698976ffb448776
Elijah-Ye <elijah.gaohan.ye@gmail.com>
2023-10-20 17:21:17 -0500

    adding ex.sv change

--- mp4/hdl/ex_stage/ex.sv
+++ mp4/hdl/ex_stage/ex.sv
@@ -12,21 +12,30 @@ import rv32i_types::*;
     /* ALU signals */
     rv32i_word alumux1_out;
     rv32i_word alumux2_out;
+    rv32i_word alu_out;
     /* CMP signals */
     logic br_en;
     rv32i_word cmpmux_out;
     logic is_jlar;
+    /* MAR signals */
+    rv32i_word marmux_out;
 
+    /* signals that pass down to the next stage */
     assign ex_out.cmp_out = br_en;
     assign is_jlar = (ex_in.ctrl_wd.opcode == op_jalr);
     assign ex_out.ctrl_wd = ex_in.ctrl_wd;
+    assign ex_out.alu_out = alu_out;
+    assign ex_out.mar = marmux_out;
+    assign ex_out.mem_data_out = ex_in.rs2_out << (8 * marmux_out[1:0]); 
+    assign ex_out.u_imm = ex_in.u_imm;
+    assign ex_out.rd = ex_in.rd;
     
 
     alu ALU(
         .aluop(ex_in.ctrl_wd.ex_ctrlwd.aluop),
         .a(alumux1_out),
         .b(alumux2_out),
-        .f(ex_out.alu_out)
+        .f(alu_out)
     );
 
     cmp CMP(
@@ -68,6 +77,12 @@ import rv32i_types::*;
             end
             default: pcmux_sel = {1'b0, br_en & ex_in.ctrl_wd.ex_ctrlwd.is_branch};
         endcase
+
+        unique case (marmux_sel)
+            marmux::pc_out: marmux_out = ex_in.ctrl_wd.pc;
+            marmux::alu_out: marmux_out = alu_out;
+        endcase
+
 
     end : EX_MUXES
 
