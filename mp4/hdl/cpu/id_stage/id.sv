4474bd65b4bd320be328b0c623373fce3e4022d0
Elijah-Ye <elijah.gaohan.ye@gmail.com>
2023-10-21 17:22:06 -0500

    done for now

--- mp4/hdl/cpu/id_stage/id.sv
+++ mp4/hdl/cpu/id_stage/id.sv
@@ -3,25 +3,39 @@ module i_decode
 import rv32i_types::*;
 (
     /* inputs */
-    input logic clk,
-    input logic rst,
-    input IF_ID_stage_t id_in,
-    input rv32i_word regfile_in,
-    input logic load_regfile
+    input logic             clk,
+    input logic             rst,
+    input IF_ID_stage_t     id_in,
+    input rv32i_word        regfile_in,
+    input logic             load_regfile,
     
     /* outputs to ID/EX buffer*/
-    output ID_EX_stage_t id_out
-
+    output ID_EX_stage_t    id_out
 );
     /* RegFile signals */
 
 
-    /* signals to send out to next stage */
+    /* control word signals */
 
+
+    /* signals to send out to next stage */
+    assign id_out.i_imm = {{21{id_in.ir.word[31]}}, id_in.ir.word[30:20]};
+    assign id_out.s_imm = {{21{id_in.ir.word[31]}}, id_in.ir.word[30:25], id_in.ir.word[11:7]};
+    assign id_out.b_imm = {{20{id_in.ir.word[31]}}, id_in.ir.word[7], id_in.ir.word[30:25], id_in.ir.word[11:8], 1'b0};
+    assign id_out.u_imm = {id_in.ir.word[31:12], 12'h000};
+    assign id_out.j_imm = {{12{id_in.ir.word[31]}}, id_in.ir.word[19:12], id_in.ir.word[20], id_in.ir.word[30:21], 1'b0};
+    assign id_out.rd = id_in.ir.r_inst.rd;
     
     /* assignments */
     
 
+
+    /* control word */
+    control_word ControlWord (
+        .pc_i(id_in.pc),
+        .instr_i(id_in.ir.word),
+        .control_words_o(id_out.ctrl_wd)
+    );
 
     /* regfile */
     regfile RegFile(
@@ -35,8 +49,7 @@ import rv32i_types::*;
         .reg_a(id_out.rs1_out),
         .reg_b(id_out.rs2_out)
     );
-    /* control word */
-
+    
     /* possible Hazard Detection Unit in forwarding */
     /* save for cp2 */
 
