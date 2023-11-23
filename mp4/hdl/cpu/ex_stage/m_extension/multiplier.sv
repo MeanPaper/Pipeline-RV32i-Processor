d80852f9567bc840e0abda5b6db463179b22c166
Dongming Liu <dl35@illinois.edu>
2023-11-23 17:09:50 -0600

    slack for multiplier is gone

--- mp4/hdl/cpu/ex_stage/m_extension/multiplier.sv
+++ mp4/hdl/cpu/ex_stage/m_extension/multiplier.sv
@@ -12,9 +12,9 @@ import rv32i_types::*;
 );
 
 
-// logic [31:0] upper_partial_sum;
-// logic [32:0] lower_partial_sum;   // 33 bits, [32] is the carry bit
-// logic [31:0] lower_reg, upper_reg;
+logic [31:0] upper_partial_sum;
+logic [32:0] lower_partial_sum;   // 33 bits, [32] is the carry bit
+logic [31:0] lower_reg, upper_reg;
 
 logic should_neg;                   // determine if one of the rs is negative
 logic [63:0] dadda_top_o, dadda_bot_o;
@@ -22,8 +22,11 @@ logic [31:0] rs1_data_tmp, rs2_data_tmp;
 logic [63:0] row_top, row_bot;      // the multiplication is going to be 64 bits long
 logic [31:0] op1, op2;              // for opA and opB 
 logic [31:0] op1_reg, op2_reg;
-// logic lower_partial_carry;
+logic lower_partial_carry;
 logic [63:0] mul_result;
+
+// logic [63:0] 
+// logic [31:0] low_half_reg, upper_half_reg;
 
 // logic should_load, should_prop;
 logic [2:0] mul_cycle, next_cycle;
@@ -37,7 +40,7 @@ logic [2:0] mul_cycle, next_cycle;
 */
 
 // 4 case bit flip
-always_comb begin
+always_comb begin : mult_pre_process
     op1 = rs1_data_tmp;
     op2 = rs2_data_tmp;
     should_neg = '0;
@@ -74,7 +77,7 @@ dadda_tree dadda_tree(
     .row_bot(dadda_bot_o)
 );
 
-always_ff @(posedge clk) begin
+always_ff @(posedge clk) begin : mult_flip_flops
     if(rst) begin    
         row_top <= '0;
         row_bot <= '0;
@@ -85,6 +88,9 @@ always_ff @(posedge clk) begin
         
         op1_reg <= '0;
         op2_reg <= '0;
+        
+        lower_reg <= '0;
+        upper_reg <= '0;
     end
     else begin
         // only allow data to float only if is_mul is trigger
@@ -94,16 +100,19 @@ always_ff @(posedge clk) begin
         op2_reg <= op2;
         row_top <= dadda_top_o;
         row_bot <= dadda_bot_o;
+        
+        lower_reg <= lower_partial_sum[31:0];
+        upper_reg <= upper_partial_sum;
     end
 end
 
 // a simple state machine start
-always_comb begin
-    mul_done = mul_cycle[2];
-    // mul_done = (mul_cycle == 3'b11);
+always_comb begin : mult_cycle_counter
+    // mul_done = mul_cycle[2];
+    mul_done = (mul_cycle == 3'b101);
     next_cycle = mul_cycle + 1'b1;
-    if(mul_cycle[2]) begin
-    // if(mul_cycle == 3'b11) begin
+    // if(mul_cycle[2]) begin
+    if(mul_cycle == 3'b101) begin
         next_cycle = '0;
     end
 end
@@ -119,13 +128,20 @@ end
 
 // a simple state machine end
 
-always_comb begin    
+always_comb begin : final_compute
 
-    // final result computation
-    mul_result = row_top + row_bot;
+    lower_partial_sum = {1'b0, row_top[31:0]} + {1'b0, row_bot[31:0]};          // lower half of the product
+    lower_partial_carry = lower_partial_sum[32];                                // the carry from the lower part 
+    upper_partial_sum = row_top[63:32] + row_bot[63:32];
+
+    // lower_partial_carry;  // upper half of the product adds with partial
     if(should_neg) begin
-        mul_result = ~(row_top + row_bot) + 1'b1;
-    end
+        lower_partial_sum = {1'b0, ~row_top[31:0]} + {1'b0, ~row_bot[31:0]} + 1'b1 + 1'b1;      // negation, one complement + 1
+        lower_partial_carry = lower_partial_sum[32];                                            // the carry from the lower part 
+        upper_partial_sum = (~row_top[63:32]) + (~row_bot[63:32]);
+    end 
+
+    mul_result = {upper_reg + lower_partial_carry, lower_reg};
 
     case(funct3)
         mulh, mulhu, mulhsu: begin  // get 32 higher bits 
@@ -135,7 +151,10 @@ always_comb begin
             mul_out = mul_result[31:0];
         end
     endcase
+
 end
 
 endmodule
+
+
 
