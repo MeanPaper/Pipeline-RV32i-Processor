8af74d7aeee03acfbc4b9ea5f254191e71f3730f
Elijah Ye <gaohany2@illinois.edu>
2023-11-29 15:22:21 -0600

    Vanilla is now sythesizable

--- mp4/hdl/cpu/ex_stage/alu.sv
+++ mp4/hdl/cpu/ex_stage/alu.sv
@@ -12,7 +12,7 @@ begin
     unique case (aluop)
         alu_add:  f = a + b;
         alu_sll:  f = a << b[4:0];
-        alu_sra:  f = $signed(a) >>> b[4:0];
+        alu_sra:  f = unsigned'(signed'(a) >>> b[4:0]);
         alu_sub:  f = a - b;
         alu_xor:  f = a ^ b;
         alu_srl:  f = a >> b[4:0];
