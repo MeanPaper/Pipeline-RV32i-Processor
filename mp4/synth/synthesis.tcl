831a879ab406057b961d3642776400079c5e5586
Dongming Liu <dl35@illinois.edu>
2023-12-04 00:07:05 -0600

    vanilla m_extension testing, used for presentation

--- mp4/synth/synthesis.tcl
+++ mp4/synth/synthesis.tcl
@@ -99,8 +99,8 @@ set_fanout_load 8 [all_outputs]
 link
 
 # 400Mhz if compile with compile_ultra 
-# compile_ultra -gate_clock -retime
-compile
+compile_ultra -gate_clock -retime
+# compile
 
 current_design $design_toplevel
 
